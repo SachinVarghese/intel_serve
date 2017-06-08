ea1fbd343b5b932a07277a49a5a1ed33990aeb21
Sachin.Varghese <sachin.varghese@mu-sigma.com>
2017-06-08 11:02:24 +0530

    synaptic XOR

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import http from 'http';
 import bp from 'body-parser';
+import {myPerceptron} from './intel.js';
 const app =express();
 app.set('port',9000);
 app.use(bp.json({limit: '10mb'}));
 app.use(bp.text());
 app.use(bp.urlencoded({extended: true}));
-app.get("/",(req,res)=>{
-  res.status(200).send("Test");
+app.get("/:x/:y",(req,res)=>{
+  res.status(200).send(myPerceptron.activate([req.params.x,req.params.y]));
 });
 http.createServer(app).listen(app.get('port'),()=>{
   console.log('Server running');
